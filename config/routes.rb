804575ab422c274ceaf653d4e3e30bcff571c82e
Vivian Ly <viv.ly.2803@gmail.com>
2024-04-17 00:00:59 -0500

    Finalized pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'home', to: 'pages#home'
+  root to: redirect('/home')
+  get 'about', to: 'pages#about'
+  get 'work', to: 'pages#work'
+  get 'contact', to: 'pages#contact'
 
   # Defines the root path route ("/")
   # root "articles#index"
